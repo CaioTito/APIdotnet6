8a3419487937adaba291a0d6b43e221b1444b6f5
Caio Tito <caio_tito@hotmail.com>
2022-02-22 10:12:58 -0300

    Primeiro Commit da API Wiz .Net 6

---
+++ src/Wiz.Template.API/ViewModels/Customer/CustomerViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Wiz.Teste1.API.ViewModels.Address;
+
+namespace Wiz.Teste1.API.ViewModels.Customer
+{
+    public class CustomerViewModel
+    {
+        [JsonConstructor]
+        public CustomerViewModel(int id, int addressId, string name)
+        {
+            Id = id;
+            AddressId = addressId;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public int AddressId { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+        public AddressViewModel Address { get; set; }
+
+
+    }
+}
